8a48d5dc9b4addd9231a071833f5037b4c9c05fc
JoSungHyoen <jsh950618@gmail.com>
2024-03-11 22:05:41 +0900

    todo state create

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import './App.css'
+import { useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className='App'>
       <Header />
